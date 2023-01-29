2e2a907bb8b43a97fad463d5adcae1e3f8843126
Jonty <17radcj@olab.org.uk>
2023-01-29 16:05:49 +0000

    colour no work

--- pong.py
+++ pong.py
@@ -1,28 +1,33 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.graphics import Color
 
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
     def bounce_ball(self, ball):
-        if self.collide_widget(ball):
+        if self.collide_widget(ball):   
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
+            return True
+        else:
+            return False
 
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+    Color = ListProperty((1, 1, 1, 1))
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
@@ -41,20 +46,27 @@ class PongGame(Widget):
         self.ball.move()
 
         # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
+        if self.player1.bounce_ball(self.ball):
+            self.ball.colour = (0, 135/255, 130/255, 1)
+
+        if self.player2.bounce_ball(self.ball):
+            self.ball.colour = (199/255,1,145/255,1)
 
         # bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
+            #self.ball.Color = (0, 1, 0 , 1)
 
         # score
         if self.ball.x < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
+            self.ball.Color = (0, 1, 0 , 1)
+            
         if self.ball.right > self.width:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
+            self.ball.Color = (0, 1, 0 , 1) 
 
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
