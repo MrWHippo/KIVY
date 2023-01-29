700fe89187d153911843a40cb391cefc8b88efe2
Jonty <17radcj@olab.org.uk>
2023-01-29 22:28:52 +0000

    Fixed Pong

--- pong.py
+++ pong.py
@@ -5,22 +5,42 @@ from kivy.properties import (
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
-from kivy.graphics import Color
+#from kivy.graphics import Color
+import keyboard
 
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
+    Color = ListProperty((1,1,1,1))
 
+    # ball bounce off of paddles
     def bounce_ball(self, ball):
         if self.collide_widget(ball):   
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+            if (vx * 1.1) < 10:
+                vel = bounced * 1.1
+                ball.velocity = vel.x, vel.y + offset
+            else:
+                vel = bounced
+                ball.velocity = vel.x, vel.y + offset
             return True
         else:
             return False
+    
+    # moving paddles with keyboard
+    def move_paddles(self, player):
+        if player == 1:
+            if keyboard.is_pressed("w"):
+                self.y += 5
+            if keyboard.is_pressed("s"):
+                self.y -= 5
+        else:
+            if keyboard.is_pressed("up"):
+                self.y += 5
+            if keyboard.is_pressed("down"):
+                self.y -= 5
 
 
 class PongBall(Widget):
@@ -37,6 +57,7 @@ class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    
 
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
@@ -44,30 +65,38 @@ class PongGame(Widget):
 
     def update(self, dt):
         self.ball.move()
+        print(self.ball.velocity)
+
+        self.player1.Color = (0, (135/255), (130/255), 1) 
+        self.player2.Color = ( (199/255),1, (145/255),1)
+
+        # keyboard
+        self.player1.move_paddles(1)
+        self.player2.move_paddles(2)
 
         # bounce of paddles
         if self.player1.bounce_ball(self.ball):
-            self.ball.colour = (0, 135/255, 130/255, 1)
+            self.ball.Color = (0, (135/255), (130/255), 1)
 
         if self.player2.bounce_ball(self.ball):
-            self.ball.colour = (199/255,1,145/255,1)
+            self.ball.Color = ( (199/255),1, (145/255),1)
 
         # bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
-            #self.ball.Color = (0, 1, 0 , 1)
 
         # score
-        if self.ball.x < self.x:
+        if (self.ball.x + 25) < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
-            self.ball.Color = (0, 1, 0 , 1)
+            #self.ball.Color = (68/255, 214/255, 44/255, 1) 
             
-        if self.ball.right > self.width:
+        if (self.ball.right - 25) > self.width:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
-            self.ball.Color = (0, 1, 0 , 1) 
+            #self.ball.Color = (68/255, 214/255, 44/255, 1) 
 
+    # moving paddles with mouse/trackpad
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
